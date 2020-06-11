9b09c184eb4b1721588cbf4997202fea81f67c2e
Jason A. Cardarelli <jcardar@umich.edu>
2020-06-11 15:01:04 -0400

    Updating Screen Definition files to include yaw, pitch, and roll definitions

--- continuous_field/source_code/screen.cpp
+++ continuous_field/source_code/screen.cpp
@@ -3,18 +3,28 @@
 #include "threevector.h"
 #include <cmath>
 
-Screen::Screen(ThreeVec position, double angle_deg, double length, double height)
-        : m_position(position), m_angle(angle_deg), m_length(length), m_height(height)
+Screen::Screen(ThreeVec position, double angle_rad_about_x, double angle_rad_about_y, double angle_rad_about_z, double length, double height)
+        : m_position(position), m_angle_about_x(angle_rad_about_x), m_angle_about_y(angle_rad_about_y), m_angle_about_z(angle_rad_about_z), m_length(length), m_height(height)
     {
-        if(angle_deg > 180)
+        if(angle_rad_about_x > M_PI)
         {
-            double multiple = angle_deg/180;
-            m_angle         = angle_deg - ( static_cast<int>(multiple)*180 );
+            double multiple = angle_rad_about_x/M_PI;
+            m_angle_about_x = angle_rad_about_x - ( static_cast<int>(multiple)*M_PI );
+        }
+        if(angle_rad_about_y > M_PI)
+        {
+            double multiple = angle_rad_about_y/M_PI;
+            m_angle_about_y = angle_rad_about_y - ( static_cast<int>(multiple)*M_PI );
+        }
+        if(angle_rad_about_z > M_PI)
+        {
+            double multiple = angle_rad_about_z/M_PI;
+            m_angle_about_z = angle_rad_about_z - ( static_cast<int>(multiple)*M_PI );
         }
     }
 
 Screen::Screen(ThreeVec position, double length, double height)
-        : m_position(position), m_angle(0.0), m_length(length), m_height(height)
+        : m_position(position), m_angle_about_x(0.0), m_angle_about_y(0.0), m_angle_about_z(0.0), m_length(length), m_height(height)
     {
     }
 
@@ -28,17 +38,98 @@ void Screen::set_pos(int index, double value)
     m_position.set(index, value);
 }
 
-void Screen::set_angle(double angle_deg)
+void Screen::set_pos(double posx, double posy, double posz)
+{
+    m_position.set(0, posx);
+    m_position.set(1, posy);
+    m_position.set(2, posz);
+}
+
+void Screen::set_angle_about_x(double angle_deg, char unit)
+{
+    if(unit == 'd')
     {
         if(angle_deg > 180)
         {
-            double multiple = angle_deg/180;
-            m_angle         = angle_deg - ( static_cast<int>(multiple)*180 );
+            double multiple  = angle_deg/180;
+            m_angle_about_x  = angle_deg - ( static_cast<int>(multiple)*180 );
+            m_angle_about_x  = m_angle_about_x * (M_PI / 180.0);
         }
         else
         {
-            m_angle = angle_deg;
-        }    
+            m_angle_about_x  = angle_deg * (M_PI / 180.0);
+        }   
+    } 
+    else //Assume radians if not 'd'
+    {
+        if(angle_deg > M_PI)
+        {
+            double multiple  = angle_deg/M_PI;
+            m_angle_about_x  = angle_deg - ( static_cast<int>(multiple)*M_PI );
+        }
+        else
+        {
+            m_angle_about_x = angle_deg;
+        } 
+    }   
+}
+
+void Screen::set_angle_about_y(double angle, char unit)
+{
+    if(unit == 'd')
+    {
+        if(angle > 180)
+        {
+            double multiple = angle/180;
+            m_angle_about_y = angle - ( static_cast<int>(multiple)*180 );
+            m_angle_about_y = m_angle_about_y * (M_PI / 180.0);
+        }
+        else
+        {
+            m_angle_about_y = angle * (M_PI / 180.0);
+        }   
+    } 
+    else //Assume radians if not 'd'
+    {
+        if(angle > M_PI)
+        {
+            double multiple  = angle/M_PI;
+            m_angle_about_y  = angle - ( static_cast<int>(multiple)*M_PI );
+        }
+        else
+        {
+            m_angle_about_y = angle;
+        } 
+    }  
+}
+
+void Screen::set_angle_about_z(double angle_deg, char unit)
+    {
+        if(unit == 'd')
+        {
+            if(angle_deg > 180)
+            {
+                double multiple = angle_deg / 180;
+                m_angle_about_z = angle_deg - ( static_cast<int>(multiple)*180 );
+                m_angle_about_z = m_angle_about_z * (M_PI / 180.0);
+            }
+            else
+            {
+                m_angle_about_z = angle_deg * (M_PI / 180.0);
+            }   
+        } 
+        else //Assume radians if not 'd'
+        {
+            if(angle_deg > M_PI)
+            {
+                double multiple = angle_deg/M_PI;
+                m_angle_about_z = angle_deg - ( static_cast<int>(multiple)*M_PI );
+            }
+            else
+            {
+                m_angle_about_z = angle_deg;
+            } 
+        }
     }
 
 void Screen::set_length(double length)
@@ -61,23 +152,59 @@ double Screen::get_pos(int index)
         return m_position.get(index);
     }
 
-double Screen::get_angle(char type)
+double Screen::get_angle_about_x(char type)
+{
+    if(type == 'd')
     {
-        if(type == 'd')
-        {
-            return m_angle;
-        }
-        else if(type == 'r')
-        {
-            return m_angle*(M_PI/180);
-        }
-        else
-        {
-            std::cout << "Incorrect Screen angle type; degrees used by default";
-            return m_angle;
-        }
-        
+        return m_angle_about_x*(180.0/M_PI);
     }
+    else if(type == 'r')
+    {
+        return m_angle_about_x;
+    }
+    else
+    {
+        std::cout << "Incorrect Screen angle_x type in get_angle_about_x(); radians used by default";
+        return m_angle_about_x;
+    }
+    
+}
+
+double Screen::get_angle_about_y(char type)
+{
+    if(type == 'd')
+    {
+        return m_angle_about_y*(180.0/M_PI);
+    }
+    else if(type == 'r')
+    {
+        return m_angle_about_y;
+    }
+    else
+    {
+        std::cout << "Incorrect Screen angle_y type in get_angle_about_y(); radians used by default";
+        return m_angle_about_y;
+    }
+    
+}
+
+double Screen::get_angle_about_z(char type)
+{
+    if(type == 'd')
+    {
+        return m_angle_about_z*(180.0/M_PI);
+    }
+    else if(type == 'r')
+    {
+        return m_angle_about_z;
+    }
+    else
+    {
+        std::cout << "Incorrect Screen angle_z type in get_angle_about_z(); radians used by default";
+        return m_angle_about_z;
+    }
+    
+}
 
 double Screen::get_length()
     {
