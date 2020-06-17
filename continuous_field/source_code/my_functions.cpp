2c870ba73818abd66f9cd8ce8343ef406e33f9c2
eemahler <59741752+eemahler@users.noreply.github.com>
2020-06-17 13:15:31 -0400

    Evan

--- continuous_field/source_code/my_functions.cpp
+++ continuous_field/source_code/my_functions.cpp
@@ -9,6 +9,8 @@
 #include "particle.h"
 #include <cmath>
 #include <cfloat>
+#include <string>
+#include <vector>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void outfile_tab(double& time, std::ofstream& out_time, ThreeVec pos, std::ofstream& out_xpos, std::ofstream& out_ypos, std::ofstream& out_zpos, ThreeVec vel, std::ofstream& out_vx, std::ofstream& out_vy, std::ofstream& out_vz)
@@ -896,5 +898,126 @@ void move_to_screens(Screen screen_t[], int num_screen, Particle particle_t)
                 continue;
             }
         }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void readUnits(std::ifstream &input_stream, std::vector<std::string> &desired_units) {
+    std::string length_unit;
+    std::string energy_unit;
+    std::string angle_unit;
+    std::string magnetic_field_unit
+
+    input_stream >> length_unit >> energy_unit >> angle_unit >> magnetic_field_unit;
+
+    desired_units = {length_unit, energy_unit, angle_unit, magnetic_field_unit};
+}
+
+int readNumOf(std::ifstream &input_stream) {
+    std::string numStr;
+    input_stream >> numStr;
+    int numInt = std::stoi(numStr);
+    
+    return numInt;
+}
+
+void readMagnet(std::ifstream &input_stream, int &magNum, std::vector<std::vector<std::vector<double>>> &magInfo) {
+    magNum = readNumOf(input_stream);
+    std::string tempStr;
+
+    //outside index goes dimensions, position, magnetic field components
+    //dimensions go width, length, height
+    for(int i=0; i<3; ++i) {
+        std::vector<std::vector<double>> tempMagSet;
+
+        for(int j=0; j<magNum; ++j) {
+            std::vector<double> tempInfoBits;
+
+            for(int k=0; k<3; ++k) {
+                input_stream >> tempStr;
+                double tempDbl = std::stod(tempStr);
+                tempInfoBits.push_back(tempDbl);
+            }
+            tempMagSet.push_back(tempInfoBits);
+        }
+        magInfo.push_back(tempMagSet);
+    }
+}
+
+void readBeam(std::ifstream &input_stream, std::vector<std::vector<double>> &beamInfo) {
+    std::string tempStr;
+
+    //outside index goes position, energy, direction
+    for(int i=0; i<3; ++i) {
+        std::vector<double> tempInfoBits;
+
+        if(i==1) {
+            input_stream >> tempStr;
+            double tempDbl = std::stod(tempStr);
+            tempInfoBits.push_back(tempDbl);
+        }
+        else {
+            for(int j=0; j<3; ++j) {
+                input_stream >> tempStr;
+                double tempDbl = std::stod(tempStr);
+                tempInfoBits.push_back(tempDbl);
+            }
+        }
+        beamInfo.push_back(tempInfoBits);
+    }
+}
+
+void readSpread(std::ifstream &input_stream, std::vector<std::vector<double>> &spreadInfo) {
+    std::string tempStr;
+
+    for(int i=0; i<3; ++i) {
+        std::vector<double> tempInfoBits;
+
+        if(i==0) {
+            for(int k=0; k<3; ++k) {
+                input_stream >> tempStr;
+                double tempDbl = std::stod(tempStr);
+                tempInfoBits.push_back(tempDbl);
+            }
+        }
+        else {
+            input_stream >> tempStr;
+            double tempDbl = std::stod(tempStr);
+            tempInfoBits.push_back(tempDbl);
+        }
+        spreadInfo.push_back(tempInfoBits);
+    }
+}
+
+void readScreen(std::ifstream &input_stream, int &screenNum, std::vector<std::vector<std::vector<double>>> &screenInfo) {
+    screenNum = readNumOf(input_stream);
+    std::string tempStr;
+
+    //outside index goes dimensions, position, angles
+    //dimensions go length, height
+    //angles go z-axis, x-axis
+    for(int i=0; i<3; ++i) {
+        std::vector<std::vector<double>> tempScreenSet;
+
+        for(int j=0; j<screenNum; ++j) {
+            std::vector<double> tempInfoBits;
+
+            if(i==0) {
+                for(int k=0; k<3; ++k) {
+                    input_stream >> tempStr;
+                    double tempDbl = std::stod(tempStr);
+                    tempInfoBits.push_back(tempDbl);
+                }
+            }
+            else {
+                for(int k=0; k<2; ++k) {
+                    input_stream >> tempStr;
+                    double tempDbl = std::stod(tempStr);
+                    tempInfoBits.push_back(tempDbl);
+                }
+            }
+            tempScreenSet.push_back(tempInfoBits);
+        }
+        screenInfo.push_back(tempScreenSet);
     }
 }
