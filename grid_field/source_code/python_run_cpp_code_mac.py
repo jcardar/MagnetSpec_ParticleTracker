f668d3db9c099b41006e9d48d8fd343a2576aa68
Jason A. Cardarelli <jcardar@umich.edu>
2020-10-31 11:36:12 -0400

    Adding general changes to the code to the github.

--- grid_field/source_code/python_run_cpp_code_mac.py
+++ grid_field/source_code/python_run_cpp_code_mac.py
@@ -3,10 +3,11 @@ import subprocess
 
 #print 'Number of arguments:', len(sys.argv), 'arguments.'
 #print 'Argument List:', str(sys.argv)
-run_args = ("./run")
-#Or just:
-#args = "bin/bar -c somefile.xml -d text.txt -r aString -f anotherString".split()
-popen = subprocess.Popen(run_args, stdout=subprocess.PIPE)
-popen.wait()
-output = popen.stdout.read()
-print(output)
+def run_spectrometer_code():
+    run_args = ("./run")
+    #Or just:
+    #args = "bin/bar -c somefile.xml -d text.txt -r aString -f anotherString".split()
+    popen = subprocess.Popen(run_args, stdout=subprocess.PIPE)
+    popen.wait()
+    output = popen.stdout.read()
+    print(output)
